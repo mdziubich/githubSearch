ad11e2fa503c66509f2fc6d01fdd369ebae203ce
Małgorzata Dziubich <mdziubich@Magorzatas-MacBook-Pro.local>
2018-02-22 22:44:07 +0100

    ad bridging header

---
+++ github_search_app/github_search_app/github_search_app-Bridging-Header.h
@@ -0,0 +1,6 @@
+#ifndef github_search__Bridging_Header_h
+#define github_search_app_Bridging_Header_h
+
+#import <UIScrollView_InfiniteScroll/UIScrollView+InfiniteScroll.h>
+
+#endif /* github_search_app_Bridging_Header_h */
